2e72b26091f6669e4910fca31094c093341180a6
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-03-30 16:53:14 -0400

    More work on SimpleLogin. Now can pass object to another page; trick is to include the class page again on the new page.

---
+++ SimpleLogin.php
@@ -0,0 +1,10 @@
+<?php
+include('BootlegZon.php');
+session_start();
+#echo "hello";
+$obj = $_SESSION['object'];
+#$obj->displayProcessing();
+$obj->displayLogin();
+
+
+?>
--- StoreFront.php
+++ StoreFront.php
@@ -15,6 +15,7 @@ $_SESSION['toonses'] = $animal;
 #error_reporting(-1);
     include('BootlegZon.php');
     $obj = new BootlegZon();
+    $_SESSION['object'] = $obj;
     if (isset($_POST["uname"])) {
       $username = $_POST["uname"];
       echo "<i>" . $username . "</i> is logged in. <p>";
@@ -33,8 +34,8 @@ $_SESSION['toonses'] = $animal;
 
     }
     else {
-      echo "<i>Users can log in at a link coming soon!</i> <p>";
-      echo '<a href = "simple_login.php">Sign in</a>';
+      echo "<i>Users can create an account at a link coming soon!</i> <p>";
+      echo '<a href = "SimpleLogin.php">Sign in</a><br>';
       $obj->displayProcessing();
       $obj->user = 'bfsmith_reader';
       $obj->password = 'Xm8av2CKT7rSG2k7';
