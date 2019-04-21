69a2e7f5ea78710cc8933e7ce498a23c1392aa88
Barry F. Smith <21973169+bfsmith9@users.noreply.github.com>
2019-04-21 14:36:42 -0400

    Updated conv scripts

--- serverscript.sh
+++ serverscript.sh
@@ -3,3 +3,7 @@
 cp ../../uvm-store/BootlegZon.php BootlegZonBarryunfixed.php
 sed -f conv2 BootlegZonBarryunfixed.php  > fix.php
 cp fix.php BootlegZonBarry.php
+
+cp ../../uvm-store/Cart.php CartBarryunfixed.php
+sed -f conv2 CartBarryunfixed.php  > fixC.php
+cp fixC.php CartBarry.php
