6c4e14b80aeefb3da342c332f8e06d530ed50096
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-04-28 11:29:29 -0400

    Minor changes - testing

---
+++ CartTestBarry.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+# Debugging
+/* print_r($_SESSION);
+echo "<p>";
+echo "just printed the whole session<br>";
+print_r($_SESSION['checkbox']);
+echo "just printed checkbox?<br>";
+echo $_SESSION['checkbox'][0];
+echo $_SESSION['checkbox'][1];
+echo "<p>"; */
+
+include('BootlegZonTestBarry.php');
+$checkBoxArray = $_POST['checkbox'];
+
+$username = $_SESSION['uname'];
+$obj = $_SESSION['object'];
+
+# Debugging
+# $kitty = $_SESSION['Toonces'];
+
+# Get the BootlegZon object stored in the $_SESSION
+$obj = $_SESSION['object'];
+
+# Debugging
+# var_dump($_SESSION);
+# var_dump($_POST);
+
+# Debugging
+/*
+echo "<p>";
+foreach($_POST['checkbox'] as $selected){
+echo "Here" . $selected."<br>";
+} */
+
+?>
+
+<!-- Start web page   Start web page   Start web page     -->
+<html lang="en">
+
+  <head>
+    <title>Nebula Knick Knacks Shopping Cart</title>
+  </head>
+
+  <body>
+
+    <?php
+    # Debugging
+    # echo "Hello, this is the cart, " . $username . "<br>";
+    # echo "And here is the identity of Toonces, " . $kitty . "<br>";
+    # echo "Hello again.<br>";
+
+    # print_r($checkBoxArray);
+    # print_r($_SESSION['checkbox']);
+
+    $obj = new BootlegZonTestBarry();
+    $obj->user = 'bfsmith_writer';
+    $obj->password = 'd7WJWjLABFHzCqv8';
+    $obj->dbase = 'BFSMITH_STORE';
+    $obj->host = '132.198.101.199';
+    $obj->port = 3306;
+
+    $obj->showCart($checkBoxArray);
+
+    echo "<p>";
+    echo "<hr>";
+    echo "<p>";
+    echo '<p><a href="StoreFront.php">Back to store</a><p>';
+
+
+    ?>
+  </body>
+</html>
--- serverscript.sh
+++ serverscript.sh
@@ -7,3 +7,7 @@ cp fix.php BootlegZonBarry.php
 cp ../../uvm-store/Cart.php CartBarryunfixed.php
 sed -f conv2 CartBarryunfixed.php  > fixC.php
 cp fixC.php CartBarry.php
+
+cp ../../uvm-store/Checkout.php CheckoutBarryunfixed.php
+sed -f conv2 CheckoutBarryunfixed.php  > fixCh.php
+cp fixCh.php CheckoutBarry.php
